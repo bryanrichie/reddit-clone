a504c5a95868c1b48e87066890a1aab311dd8352
Bryan Lee <bryanrichie97@gmail.com>
2022-05-30 15:24:36 +1000

    feat(homepage): finished styling homepage to display all posts

--- src/client/pages/HomePage.tsx
+++ src/client/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import {
   AspectRatio,
   Box,
   Flex,
+  HStack,
   Image,
   ListItem,
   Text,
@@ -10,6 +11,7 @@ import {
   useColorModeValue,
   VStack,
 } from '@chakra-ui/react';
+import { MdArrowCircleDown, MdArrowCircleUp, MdOutlineModeComment, MdLink } from 'react-icons/md';
 import { NavBar } from '../components/NavBar';
 import { useGetPosts } from '../hooks/useGetPosts';
 import _ from 'lodash';
@@ -18,32 +20,85 @@ export const HomePage = () => {
   const { data } = useGetPosts();
 
   const bodyBg = useColorModeValue('blue.200', 'blue.900');
+  const postBg = useColorModeValue('white', 'gray.700');
+  const postBorder = useColorModeValue('gray.700', 'white');
+  const overflowGradient = useColorModeValue(
+    'linear-gradient(transparent 200px, white)',
+    'linear-gradient(transparent 200px, #2D3748)'
+  );
 
   const posts = _.map(data, (post) => {
     const isTitleOnlyPost = !post.text && !post.url;
     const content =
       post.text && !post.url ? (
-        <Text>{post.text}</Text>
+        <Text
+          pt={3}
+          maxH={'300px'}
+          overflow="hidden"
+          position={'relative'}
+          _before={{
+            content: `''`,
+            w: `100%`,
+            h: `100%`,
+            position: `absolute`,
+            left: `0`,
+            top: `0`,
+            background: `${overflowGradient}`,
+          }}
+        >
+          {post.text}
+        </Text>
       ) : (
-        <Image alignSelf={'center'} maxH={'300px'} objectFit={'cover'} src={post.url ?? ''} />
+        <Image
+          pt={3}
+          alignSelf={'center'}
+          maxH={'300px'}
+          objectFit={'cover'}
+          src={post.url ?? ''}
+        />
       );
 
     return (
-      <ListItem key={post.id} listStyleType={'none'} p={5} borderRadius="md" bg={'white'}>
-        <Flex>
-          <VStack align={'left'} w={'100%'}>
+      <ListItem
+        key={post.id}
+        listStyleType={'none'}
+        p={5}
+        borderRadius="md"
+        bg={postBg}
+        border="1px"
+        borderColor={postBorder}
+      >
+        <HStack justify={'space-between'} spacing={5}>
+          <VStack alignSelf="flex-start">
+            <MdArrowCircleUp size={'40px'} />
+            <Text>0</Text>
+            <MdArrowCircleDown size={'40px'} />
+          </VStack>
+          <VStack align={'left'} w={'100%'} spacing={0} alignSelf={'flex-start'}>
             <Text fontWeight={'extrabold'} fontSize={'2xl'}>
               {_.upperFirst(post.title)}
             </Text>
             {!isTitleOnlyPost ? content : null}
           </VStack>
-        </Flex>
+        </HStack>
+        <HStack pt={5} fontWeight="bold" ml={'60px'} spacing={10}>
+          <HStack>
+            <MdOutlineModeComment color="#718096" size={'20px'} />
+            <Text color={'gray.500'}>Comments</Text>
+          </HStack>
+          <HStack>
+            <MdLink color="#718096" size={'20px'} />
+            <Text color={'gray.500'} size={'20px'}>
+              Share
+            </Text>
+          </HStack>
+        </HStack>
       </ListItem>
     );
   }).reverse();
 
   return (
-    <Flex bg={bodyBg} h={'100vh'} flexDir="column">
+    <Flex bg={bodyBg} minH={'100vh'} flexDir="column" pb={5}>
       <NavBar />
       <UnorderedList
         alignSelf={'center'}
